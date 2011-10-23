2cce896bc475a49ec6ee3054c19ad783b0576fa5
Shreyans Bhansali <shreyans@venmo.com>
2011-10-22 21:49:56 -0400

    renaming

--- dispense_treat.py
+++ dispense_treat.py
@@ -2,14 +2,14 @@
 import sys
 import serial
 
-def main(usbmodem_number):
+def run_arduino(usbmodem_number):
     # run the code already on the arduino
     serial.Serial('/dev/tty.usbmodem%s' % usbmodem_number, 9600)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         try:
-            main(int(sys.argv[1]))
+            run_arduino(int(sys.argv[1]))
         except:
             pass
 
